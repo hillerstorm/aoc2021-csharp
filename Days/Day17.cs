50b326301825be536af405861febd86623cc83c9
Johan Hillerström <jh@woocode.com>
2021-12-18 14:35:44 +0100

    Formatting

--- Days/Day17.cs
+++ Days/Day17.cs
@@ -1,9 +1,7 @@
 namespace Aoc2021;
 
-public class Day17 : IDay
-{
-  public (Func<string> Part1, Func<string> Part2) Parts(string rawInput)
-  {
+public class Day17 : IDay {
+  public (Func<string> Part1, Func<string> Part2) Parts(string rawInput) {
     return (
       () => Part1(rawInput.Trim()).ToString(),
       () => Part2(rawInput.Trim()).ToString()
@@ -16,8 +14,7 @@ public class Day17 : IDay
   public static int Part2(string input) =>
     Run(input, Part.Two);
 
-  public static int Run(string input, Part part)
-  {
+  public static int Run(string input, Part part) {
     var (xMin, xMax, _) = input[15..input.IndexOf(",")].Split("..");
     var (yMin, yMax, _) = input[(input.IndexOf("y") + 2)..].Split("..");
 
@@ -29,33 +26,28 @@ public class Day17 : IDay
     var maxHeight = -1;
     var hitCount = 0;
 
-    for (var x = 0; x <= x2; x++)
-    {
-      for (var y = y1; y < Math.Abs(y1); y++)
-      {
+    for (var x = 0; x <= x2; x++) {
+      for (var y = y1; y < Math.Abs(y1); y++) {
         var xVelocity = x;
         var yVelocity = y;
         var pos = (x: 0, y: 0);
         var height = 0;
         var hit = false;
 
-        while (true)
-        {
+        while (true) {
           pos = (pos.x + xVelocity, pos.y + yVelocity);
 
           if (pos.y > height)
             height = pos.y;
 
-          xVelocity += xVelocity switch
-          {
+          xVelocity += xVelocity switch {
             > 0 => -1,
             < 0 => 1,
             _ => 0
           };
           yVelocity -= 1;
 
-          if (pos.x >= x1 && pos.x <= x2 && pos.y >= y1 && pos.y <= y2)
-          {
+          if (pos.x >= x1 && pos.x <= x2 && pos.y >= y1 && pos.y <= y2) {
             hit = true;
             hitCount++;
             break;
